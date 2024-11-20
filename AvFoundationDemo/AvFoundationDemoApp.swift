a757f8d45fca04063fde36201de4474afbb4e4ff
Md Shafiq Ullah <shohagbit0404@gmail.com>
2024-11-20 12:24:11 -0800

    Initial Commit

---
+++ AvFoundationDemo/AvFoundationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvFoundationDemoApp.swift
+//  AvFoundationDemo
+//
+//  Created by test on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AvFoundationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvFoundationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvFoundationDemo
+//
+//  Created by test on 11/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
